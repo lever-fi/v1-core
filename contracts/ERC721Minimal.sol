74564ad2ef072c683ecd24ca45bb1a6aab0584cc
prathik <37804760+prmali@users.noreply.github.com>
2022-07-06 15:08:46 -0500

    Update ERC721Minimal.sol

--- contracts/ERC721Minimal.sol
+++ contracts/ERC721Minimal.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "./interfaces/IERC721Minimal.sol";
+import {IERC721Minimal} from "./interfaces/IERC721Minimal.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 // only owner
@@ -11,7 +11,7 @@ contract ERC721Minimal is ERC721Enumerable {
     {}
 
     function mint(address target, uint256 count) external {
-        for (uint256 tokenId = 0; tokenId < count; tokenId ++) {
+        for (uint256 tokenId = 0; tokenId < count; tokenId++) {
             _mint(target, tokenId);
         }
     }
